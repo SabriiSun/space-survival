35478c4db34494f7267881f7c2270d1179c7a6a6
Sabrina Sousa <sabrinavsousa14@gmail.com>
2022-10-01 17:57:29 -0400

    Added instructions that pop up before game

--- main_game.py
+++ main_game.py
@@ -13,6 +13,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Space Survivor')
 
 font = pygame.font.SysFont("arialblack", 30)
+fontsmall = pygame.font.SysFont("arialblack", 15)
 TEXT_COL = (255, 255, 255)
 def draw_text(text, font, text_col, x, y):
     image = font.render(text, True, text_col)
@@ -28,17 +29,36 @@ ground_image = pygame.image.load('images/ground.png')
 # game needs a loop to run in
 run = True
 title_screen = True
-instruction_screen = True
+instruction_screen = False
 while run:
     
     # constantly display images for visuals
     screen.blit(bg_image, (0,0))
     if title_screen:
-        screen.blit(title_image, (310,250))
-        draw_text("Press SPACE to start", font, TEXT_COL, 450, 350)
+        # displays title screen
+        title_rect = title_image.get_rect(center=(screen_width/2, screen_height/2))
+        screen.blit(title_image, title_rect)
+        start_text = font.render("Press SPACE to start", True, TEXT_COL)
+        start_rect = start_text.get_rect(center=(screen_width/2, screen_height/2+75))
+        screen.blit(start_text, start_rect)
     if title_screen == False:
+        # set up for game visuals
         screen.blit(ground_image, (0, 600))
         screen.blit(base_image, (0,395))
+    if instruction_screen:
+        # displays game controls
+        instructions = ["Mission: Protect the Moon Base AT ALL COSTS","Use A to move left",
+        "Use D to move right","Use W to lauch rockets","Press SPACE to begin"]
+        # i and j are offsets to list instructions
+        i = -75
+        j = -25
+        for string in instructions:
+            text = font.render(string, True, TEXT_COL)
+            text_rect = text.get_rect(center=(screen_width/2, screen_height/2+i+j))
+            screen.blit(text, text_rect)
+            i+=25
+            j=0
+
     for event in pygame.event.get():
         # first event is to close game for completenes
         if event.type == pygame.QUIT:
@@ -46,7 +66,11 @@ while run:
         #keydown means a key has been pressed
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                title_screen = False
+                if title_screen:
+                    title_screen = False
+                    instruction_screen = True
+                else:
+                    instruction_screen = False
 
 
     pygame.display.update()
